bd04aa86a8f0eb0ed7af864995722bcc002b782e
vihong <probamaths@gmail.com>
2020-12-28 05:39:34 +0100

    :recycle: F-009: Add AppForm

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
-import { StyleSheet } from 'react-native';
-import { Formik } from 'formik';
+import { StyleSheet, View } from 'react-native';
 import * as Yup from 'yup';
 
 import Screen from '../components/atoms/Screen';
@@ -8,6 +7,7 @@ import Logo from '../components/molecules/Logo';
 import colorPalette from '../config/colorPalette';
 import AppField from '../components/molecules/AppField';
 import SubmitButton from '../components/molecules/SubmitButton';
+import AppForm from '../components/organisms/AppForm';
 
 const validationSchema = Yup.object().shape({
 	email    : Yup.string().required().email().label('Email'),
@@ -22,40 +22,38 @@ function LoginScreen(props) {
 				image={require('../assets/images/boeTiePrimary.png')}
 			/>
 
-			<Formik
-				initialValues={{ email: '', password: '' }}
-				onSubmit={(values) => console.log(values)}
-				validationSchema={validationSchema}
-			>
-				{() => (
-					<Fragment>
-						<AppField
-							name="email"
-							style={styles.textInputAtom}
-							icon="email"
-							placeholder="Email"
-							keyboardType="email-address"
-							textContentType="emailAddress"
-							autoCapitalize="none"
-						/>
-						<AppField
-							name="password"
-							style={styles.textInputAtom}
-							icon="lock"
-							placeholder="Password"
-							textContentType="password"
-							autoCapitalize="none"
-							autoCorrect={false}
-							secureTextEntry
-						/>
-						<SubmitButton
-							label="login"
-							color={colorPalette.white}
-							backgroundColor={colorPalette.primary}
-						/>
-					</Fragment>
-				)}
-			</Formik>
+			<View style={styles.form}>
+				<AppForm
+					initialValues={{ email: '', password: '' }}
+					onSubmit={(values) => console.log(values)}
+					validationSchema={validationSchema}
+				>
+					<AppField
+						name="email"
+						style={styles.textInputAtom}
+						icon="email"
+						placeholder="Email"
+						keyboardType="email-address"
+						textContentType="emailAddress"
+						autoCapitalize="none"
+					/>
+					<AppField
+						name="password"
+						style={styles.textInputAtom}
+						icon="lock"
+						placeholder="Password"
+						textContentType="password"
+						autoCapitalize="none"
+						autoCorrect={false}
+						secureTextEntry
+					/>
+					<SubmitButton
+						label="login"
+						color={colorPalette.white}
+						backgroundColor={colorPalette.primary}
+					/>
+				</AppForm>
+			</View>
 		</Screen>
 	);
 }
@@ -71,5 +69,8 @@ const styles = StyleSheet.create({
 	},
 	textInputAtom : {
 		color : 'grey'
+	},
+	form          : {
+		marginVertical : 40
 	}
 });
