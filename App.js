eb770bb2d04f031951f296fb555de99e3ff2f1df
vihong <probamaths@gmail.com>
2021-01-01 05:08:12 +0100

    ✨  F-013: NAVIGATION > Add TabNavigator

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import WelcomeScreen from './app/components/screens/WelcomeScreen';
 import LoadingScreen from './app/components/screens/LoadingScreen';
 import ViewImageScreen from './app/components/screens/ViewImageScreen';
 import FeedScreen from './app/components/screens/FeedScreen';
@@ -10,6 +9,10 @@ import InputScreen from './app/components/screens/InputScreen';
 import LoginScreen from './app/components/screens/LoginScreen';
 import RegisterScreen from './app/components/screens/RegisterScreen';
 import ProductEditScreen from './app/components/screens/ProductEditScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigator from './app/components/navigation/AuthNavigator';
+import navigationTheme from './app/components/navigation/navigationTheme';
+import TabNavigator from './app/components/navigation/TabNavigator';
 
 export default function App() {
 	// return <WelcomeScreen />;
@@ -19,9 +22,15 @@ export default function App() {
 	// return <ProductDetailsScreen />;
 	// return <MesssagesScreen />;
 	// return <MyAccountScreen />;
-	return <InputScreen />;
+	// return <InputScreen />;
 	// return <LoginScreen />;
 	// return <RegisterScreen />;
 	// return <ProductEditScreen />;
+	return (
+		<NavigationContainer theme={navigationTheme}>
+			<TabNavigator />
+			{/* <AuthNavigator /> */}
+		</NavigationContainer>
+	);
 	// return null;
 }
---
+++ app/components/navigation/TabNavigator.js
@@ -0,0 +1,74 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import colorPalette from '../../config/colorPalette';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Screen from '../atoms/Screen';
+import FeedStackNavigator from './FeedStackNavigator';
+import ProductEditScreen from '../screens/ProductEditScreen';
+import MyAccountScreen from '../screens/MyAccountScreen';
+
+const Tab = createBottomTabNavigator();
+export default function AppTabNavigator() {
+	return (
+		<Tab.Navigator
+			tabBarOptions={{
+				activeBackgroundColor   : colorPalette.danger,
+				activeTintColor         : colorPalette.white,
+				inactiveBackgroundColor : colorPalette.lightgrey,
+				inactiveTintColor       : colorPalette.black
+			}}
+		>
+			<Tab.Screen
+				name="Feed"
+				component={FeedStackNavigator}
+				options={{
+					tabBarIcon : ({ size, color }) => (
+						<MaterialCommunityIcons
+							name="home"
+							size={size}
+							color={color}
+						/>
+					)
+				}}
+			/>
+			<Tab.Screen
+				name="ProductEdit"
+				component={ProductEditScreen}
+				options={{
+					tabBarIcon : ({ size, color }) => (
+						<MaterialCommunityIcons
+							name="plus-circle-outline"
+							size={size}
+							color={color}
+						/>
+					)
+				}}
+			/>
+			<Tab.Screen
+				name="Account"
+				component={MyAccountScreen}
+				options={{
+					tabBarIcon : ({ size, color }) => (
+						<MaterialCommunityIcons
+							name="account"
+							size={size}
+							color={color}
+						/>
+					)
+				}}
+			/>
+		</Tab.Navigator>
+	);
+}
+
+const styles = StyleSheet.create({});
+
+const Account = () => {
+	return (
+		<Screen>
+			<Text>Account</Text>
+		</Screen>
+	);
+};
