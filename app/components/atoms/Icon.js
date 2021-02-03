426a31edbbe12029941fd133b438a03c5f6a9c59
vihong <probamaths@gmail.com>
2021-02-03 04:45:13 +0100

    ✨  F-025: FEED > Add ListFooterComponent to reload

--- app/components/atoms/Icon.js
+++ app/components/atoms/Icon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { View } from 'react-native';
 
-export default function Icon({ style, ...restProps }) {
+export default function Icon({ icon, color, backgroundColor, style, ...restProps }) {
 	return (
 		<View style={style}>
 			<MaterialCommunityIcons {...restProps} />
--- app/components/screens/FeedScreen.js
+++ app/components/screens/FeedScreen.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View, ActivityIndicator as Popo, ScrollView } from 'react-native';
 import colorPalette from '../../config/colorPalette';
 import Screen from '../atoms/Screen';
 import Card from '../molecules/Card';
@@ -13,8 +13,18 @@ import { gql, useMutation, useQuery } from '@apollo/client';
 import { formatMontant } from '../../utils/maths';
 import Text from '../atoms/Text';
 import { CREATE_PRODUCT, GET_PRODUCTS } from '../../graphql/Queries';
+import Icon from '../atoms/Icon';
+import IconInCircle from '../atoms/IconInCircle';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import LottieView from 'lottie-react-native';
 
 function FeedScreen({ navigation }) {
+	const [
+		showIndicator,
+		setShowIndicator
+	] = useState(false);
+
 	const [
 		products,
 		setProducts
@@ -40,8 +50,6 @@ function FeedScreen({ navigation }) {
 		refetch
 	} = useQuery(GET_PRODUCTS);
 
-	const nouveauProduit = {};
-
 	useEffect(
 		() => {
 			if (dataProducts) setProducts(dataProducts.products);
@@ -61,6 +69,13 @@ function FeedScreen({ navigation }) {
 		console.log('response: ', response);
 	};
 
+	const _listEmptyComponent = () => {
+		return (
+			<View>
+				<MaterialCommunityIcons name="email" size={40} color={colorPalette.primary} />
+			</View>
+		);
+	};
 	if (loadingProducts) return <ActivityIndicator visible />;
 	if (errorProducts)
 		return (
@@ -94,6 +109,38 @@ function FeedScreen({ navigation }) {
 					)}
 					refreshing={isRefresh}
 					onRefresh={() => refetch()}
+					ListFooterComponent={() =>
+						products.length > 0 && (
+							<Fragment>
+								{showIndicator ? (
+									<View style={styles.indicatorContainer}>
+										<LottieView
+											source={require('../../assets/animations/wait.json')}
+											autoPlay
+											loop
+											style={styles.indicator}
+										/>
+									</View>
+								) : (
+									<TouchableWithoutFeedback
+										onPress={async () => {
+											console.log('pokemon');
+											setShowIndicator(true);
+											await refetch();
+											setShowIndicator(false);
+										}}
+									>
+										<View style={styles.reloadContainer}>
+											<MaterialCommunityIcons
+												name="refresh"
+												size={40}
+												color={colorPalette.primary}
+											/>
+										</View>
+									</TouchableWithoutFeedback>
+								)}
+							</Fragment>
+						)}
 				/>
 			</Screen>
 		</Fragment>
@@ -101,16 +148,38 @@ function FeedScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-	screen        : {
+	screen             : {
 		backgroundColor : colorPalette.backgroundGrey,
 		paddingTop      : 20
 	},
-	cards         : {
+	cards              : {
 		paddingHorizontal : 20
 	},
-	requestFailed : {
-		alignItems : 'center',
-		padding    : '5%'
+	requestFailed      : {
+		alignItems  : 'center',
+		padding     : '5%',
+		borderWidth : 1
+	},
+	reloadContainer    : {
+		justifyContent : 'center',
+		alignItems     : 'center',
+		width          : '100%',
+		marginTop      : 30,
+		marginBottom   : 70,
+		height         : 80
+		// borderWidth    : 1
+	},
+	indicatorContainer : {
+		width        : '100%',
+		alignItems   : 'center',
+		// borderWidth : 1,
+		marginTop    : 30, // doit être de la même marginTop que reloadContainer
+		marginBottom : 70,
+		height       : 80
+	},
+	indicator          : {
+		// borderWidth : 1,
+		// borderColor : 'red'
 	}
 });
 
