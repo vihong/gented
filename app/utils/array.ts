e638aadb456dd085f82d9c6e6453a75497ad70e0
vihong <probamaths@gmail.com>
2021-04-18 17:54:46 +0200

    ✅ F-032: Test array.test.ts

---
+++ app/utils/array.ts
@@ -0,0 +1,3 @@
+export function findIndexOfWord(words: string[], wordToFind: string) {
+	return words.findIndex((option) => option === wordToFind)
+}
--- app/utils/user.js
+++
@@ -1,5 +0,0 @@
-import _ from 'lodash';
-
-export function getUsername(route) {
-	return _.isEmpty(route.params) ? 'Danny' : route.params.username;
-}
